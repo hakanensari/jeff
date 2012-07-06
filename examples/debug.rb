f72aa9fdc91d40c2458a53901d7ffe04fd804542
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-07-06 13:55:55 +0100

    Add a short script to debug with an actual AWS endpoint

---
+++ examples/debug.rb
@@ -0,0 +1,41 @@
+$:.unshift File.expand_path '../../lib', __FILE__
+
+require 'yaml'
+
+require 'jeff'
+require 'pry'
+require 'pry-doc'
+
+class Client
+  include Jeff
+
+  params  'AssociateTag' => -> { tag },
+          'Service'      => 'AWSECommerceService',
+          'Version'      => '2011-08-01'
+
+  attr_accessor :tag
+
+  def initialize
+    self.key      = config['key']
+    self.secret   = config['secret']
+    self.tag      = config['tag']
+    self.endpoint = 'http://ecs.amazonaws.com/onca/xml'
+  end
+
+  def find(asins)
+    params = {
+      'Operation' => 'ItemLookup',
+      'ItemId'    => Array(asins).join(',')
+    }
+
+    get query: params
+  end
+
+  private
+
+  def config
+    @config ||= YAML.load_file File.expand_path '../amazon.yml', __FILE__
+  end
+end
+
+binding.pry
